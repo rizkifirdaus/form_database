4ce5a55d0e317fbfacbc00383ed03490d4ba10dd
Muhammad Rizki Firdaus <rizkfirdaus@gmail.com>
2019-11-22 08:22:29 +0700

    Create proses.php

---
+++ proses.php
@@ -0,0 +1,9 @@
+<form action="proses.php" method="get">
+   Nama: <input type="text" name="Nama" />
+   <br />
+   E-Mail: <input type="text" name="E-Mail" />
+   <br />
+   <input type="submit" value="Proses Data" >
+  </form>
+
+# Meninput data kedalam database sebagai calling script ke database
